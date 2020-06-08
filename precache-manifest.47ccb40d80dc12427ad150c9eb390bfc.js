1914e40a36103b9ca0ca9c946b809d8de1608e29
Kee Tee <tkeeching@gmail.com>
2020-06-08 13:55:45 +1000

    Updates

---
+++ precache-manifest.47ccb40d80dc12427ad150c9eb390bfc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bd21576f74f3152f565f721cf354412",
+    "url": "/burger/index.html"
+  },
+  {
+    "revision": "dbc6fa9149b9ffd06aaa",
+    "url": "/burger/static/css/main.61082104.chunk.css"
+  },
+  {
+    "revision": "f5025aa9d7c85f02a3e4",
+    "url": "/burger/static/js/2.0e2c5e4e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/burger/static/js/2.0e2c5e4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbc6fa9149b9ffd06aaa",
+    "url": "/burger/static/js/main.d4973333.chunk.js"
+  },
+  {
+    "revision": "75ab14ff4938c2d16ae9",
+    "url": "/burger/static/js/runtime-main.a489e444.js"
+  }
+]);
